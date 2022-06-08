19e355e0115547100fc79031ec11ffcbf259fa71
Pat Brown <mrpatbrown@gmail.com>
2022-06-09 09:52:10 +1000

    Remove ApiClient's dependency on URLSession (#19)
    * Added short sleep to upload tests
    
    * Wrapped `URLSession` for easier testing of `ApiClient`

---
+++ Sources/Core/ApiClient/ApiSession.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
+struct ApiSession {
+  var data: (URLRequest) async throws -> (Data, URLResponse)
+
+  func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+    try await self.data(request)
+  }
+}
+
+extension ApiSession {
+  static func urlSession(_ session: URLSession) -> ApiSession {
+    ApiSession { request in
+      #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+      if #available(macOS 12, iOS 15, tvOS 15, watchOS 8, *) {
+        return try await session.data(for: request)
+      }
+      #endif
+
+      var dataTask: URLSessionDataTask?
+      let cancel: () -> Void = { dataTask?.cancel() }
+
+      return try await withTaskCancellationHandler(
+        handler: { cancel() },
+        operation: {
+          try await withCheckedThrowingContinuation { continuation in
+            dataTask = session.dataTask(with: request) { data, response, error in
+              if let data = data, let response = response {
+                continuation.resume(returning: (data, response))
+              } else {
+                continuation.resume(throwing: error ?? URLError(.badServerResponse))
+              }
+            }
+            dataTask?.resume()
+          }
+        }
+      )
+    }
+  }
+}
