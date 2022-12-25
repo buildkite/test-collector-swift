6e39b387462558d772818bd91b504191b34eaa29
Pat Brown <mrpatbrown@gmail.com>
2022-12-26 10:02:53 +1100

    Remove sleep helper

--- Tests/CoreTests/UploadClientTests.swift
+++ Tests/CoreTests/UploadClientTests.swift
@@ -12,7 +12,6 @@ final class UploadClientTests: XCTestCase {
     let trace = Trace(id: "id", history: .init(section: "section"))
 
     Task { try await uploadClient.upload(trace: trace) }
-    self.sleep(for: 0.01)
     uploadClient.waitForUploads()
 
     self.wait(for: [uploadCompleted], timeout: 0)
@@ -25,7 +24,6 @@ final class UploadClientTests: XCTestCase {
     let trace = Trace(id: "id", history: .init(section: "section"))
 
     let task = Task { try await uploadClient.upload(trace: trace) }
-    self.sleep(for: 0.01)
     uploadClient.waitForUploads(timeout: 0.1)
     task.cancel()
 
