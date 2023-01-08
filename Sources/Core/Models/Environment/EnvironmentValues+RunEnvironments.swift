7dd5d6f3d7b837feff70e51ff3ed742dc5954479
Pat Brown <mrpatbrown@gmail.com>
2023-01-09 09:57:14 +1100

    Formatting

--- Sources/Core/Models/Environment/EnvironmentValues+RunEnvironments.swift
+++ Sources/Core/Models/Environment/EnvironmentValues+RunEnvironments.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension EnvironmentValues {
   func runEnvironment(defaultKey: String = UUID().uuidString) -> RunEnvironment {
-    let ciEnv = self.buildKite
+    let ciEnv = self.buildkite
       ?? self.gitHubActions
       ?? self.circleCi
       ?? self.xcodeCloud
@@ -25,7 +25,7 @@ extension EnvironmentValues {
     )
   }
 
-  private var buildKite: RunEnvironment? {
+  private var buildkite: RunEnvironment? {
     guard let buildId = self.buildkiteBuildId else { return nil }
 
     logger?.debug("Successfully found Buildkite RunEnvironment")
@@ -104,7 +104,7 @@ extension EnvironmentValues {
     guard
       let commitHash = self.xcodeCommitSha,
       let buildNumber = self.xcodeBuildNumber,
-      let buildID = self.xcodeBuildID,
+      let buildID = self.xcodeBuildId,
       let workflowName = self.xcodeWorkflowName
     else { return nil }
 
@@ -115,8 +115,8 @@ extension EnvironmentValues {
     return RunEnvironment(
       ci: "xcodeCloud",
       key: buildID,
-      url: xcodePullRequestURL,
-      branch: xcodeBranch,
+      url: self.xcodePullRequestURL,
+      branch: self.xcodeBranch,
       commitSha: commitHash,
       number: buildNumber,
       message: message
