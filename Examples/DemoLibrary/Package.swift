cb7ccde77578e90b6dca6a0fcf67311b9b10cfd5
Pat Brown <mrpatbrown@gmail.com>
2023-01-09 11:11:17 +1100

    Compile Demo library in release mode

--- Examples/DemoLibrary/Package.swift
+++ Examples/DemoLibrary/Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
   name: "DemoLibrary",
+  platforms: [
+    .macOS("10.15"),
+    .iOS("13.0"),
+    .tvOS("13.0"),
+    .watchOS("6.0")
+  ],
   products: [
     .library(name: "DemoLibrary", targets: ["DemoLibrary"])
   ],
