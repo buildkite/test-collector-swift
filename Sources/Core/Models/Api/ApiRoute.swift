400708eca018fd5ebcea923cc2fa168451147665
Paul Annesley <paul@annesley.cc>
2025-01-23 12:26:48 +1030

    Docs: use “Test Engine” not “Analytics” where possible
    This doesn't change anything functional e.g. environment variable names,
    just documentation and code comments.

--- Sources/Core/Models/Api/ApiRoute.swift
+++ Sources/Core/Models/Api/ApiRoute.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-/// Different endpoints for the Buildkite analytics service
+/// Different endpoints for Buildkite Test Engine
 ///
 /// Currently there is only a single upload endpoint, in the future this could be expanded to include others though. The new route would be handled in
 /// `ApiClient`'s `makeRequest` function
